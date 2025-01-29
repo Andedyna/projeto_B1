ec1905b8a6a6ac4206e4c68f4bc86ce8566432dc
Anderson Nascimento <anderson.aje57@gmail.com>
2025-01-28 21:27:23 -0300

    Adicionado servidor básico com Express.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello, Worlde!");
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req,res) => {
+    res.send('Servidor rodando com Express!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
